d83c4b7700179558c216ff8d4a8f8b2bc000e7ea
GigaArchitect <nsar90554046@gmail.com>
2024-11-08 20:47:41 +0200

    oh hey tanks

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash 
-export BROWSER=/usr/bin/brave &
 mouse_id=$(xinput list | grep "HID 1bcf:08a0 Mouse" | grep -o "id=[0-9]*" | grep -o "[0-9]*")
 prop=$(xinput list-props $mouse_id | grep "libinput Accel Speed ([0-9]*):" | grep -o "[0-9]*" | head -n1)
 xinput set-prop $mouse_id $prop -1 &
@@ -8,10 +7,11 @@ copyq &
 #lxsession &
 dunst &
 udiskie &
+light -N 5 &
 numlockx on &
 blueman-applet &
 spectacle -s &
-xrandr --output eDP-1 --gamma .85:.85:.82 &
+#xrandr --output eDP-1 --gamma .85:.85:.82 &
 #xrandr --output eDP-1 --gamma .85 &
 picom &
 nm-applet &
